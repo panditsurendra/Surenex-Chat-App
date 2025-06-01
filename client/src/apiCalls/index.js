e58c03e9251429f435fa226a72866f56dec63a8a
Surendra Kumar <panditsurendrakumar23@gamil.com>
2025-06-01 23:05:28 +0530

    Update client folder with latest changes

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const url = "https://quick-chat-app-rql3.onrender.com";
-export const url = "http://localhost:4000";
+export const url = "https://surenex-chat-app-server.onrender.com";
 
 export const axiosInstance = axios.create({
     headers: {
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -5,7 +5,7 @@ import Sidebar from "./components/sidebar";
 import { io } from 'socket.io-client';
 import { useEffect, useState } from "react";
 
- const socket = io('http://localhost:4000');
+ const socket = io('https://surenex-chat-app-server.onrender.com');
 
 function Home(){
     const { selectedChat, user } = useSelector(state => state.userReducer);
